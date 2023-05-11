b593d82d50224b143a8c6dd63d3c003b01bfcab5
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-11 12:57:09 +0400

    Added page urls

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,2 +1,10 @@
-package config;public class AppConfig {
+package config;
+
+public class AppConfig {
+    public static final String DOMAIN = "https://stellarburgers.nomoreparties.site";
+    public static final String MAIN_PAGE_URL = DOMAIN + "/";
+    public static final String LOGIN_PAGE_URL = DOMAIN + "/login";
+    public static final String REGISTER_PAGE_URL = DOMAIN + "/register";
+    public static final String PROFILE_PAGE_URL = DOMAIN + "/account/profile";
+
 }
