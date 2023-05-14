ee9ce58044781f33ccc9ef8030ecc29386d7a69f
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-14 17:54:02 +0400

    Created Login page and login tests

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,2 +1,54 @@
-package pages;public class LoginPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import user.User;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    //Заголовок "Вход"
+    private final By headerLogin = By.xpath(".//h2[text()='Вход']");
+    //Поле "Email"
+    private final By inputEmail = By.xpath(".//label[text()='Email']/../input");
+    //Поле "Пароль"
+    private final By inputPassword = By.xpath(".//label[text()='Пароль']/../input");
+    //Кнопка "Войти"
+    private final By btnLogin = By.xpath(".//button[text()='Войти']");
+    //Кнопка "Восстановить пароль"
+    private final By textResetPassword = By.xpath(".//a[text()='Восстановить пароль']");
+    //Кнопка "Личный кабинет"
+    private final By btnProfile = By.xpath(".//p[text()='Личный Кабинет']");
+
+    public  LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getLoginTextFromHeader() {
+        return driver.findElement(headerLogin).getText();
+    }
+
+    public void setEmail(String email) {
+        driver.findElement(inputEmail).sendKeys(email);
+    }
+
+    public void setPassword(String password) {
+        driver.findElement(inputPassword).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(btnLogin).click();
+    }
+
+    public void clickProfileButton() {
+        driver.findElement(btnProfile).click();
+    }
+
+    public void loginUser(User user){
+        setEmail(user.getEmail());
+        setPassword(user.getPassword());
+        clickLoginButton();
+        clickProfileButton();
+    }
 }
--- src/test/java/LogoutTest.java
+++ src/test/java/LogoutTest.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class LogoutTest {
+import config.AppConfig;
+import config.BaseURI;
+import config.WebDriverConfig;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+import pages.ProfilePage;
+import user.User;
+import user.UserOperations;
+import utils.Generator;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class LogoutTest {
+    private WebDriver driver;
+    User user;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = BaseURI.BASE_URI;
+        user = Generator.generateUser();
+        UserOperations.createUser(user);
+
+        driver = WebDriverConfig.setDriver();
+        driver.manage().timeouts().implicitlyWait(WebDriverConfig.WAIT_SEC_TIMEOUT, TimeUnit.SECONDS);
+        driver.navigate().to(AppConfig.MAIN_PAGE_URL);
+
+    }
+
+    @Test
+    @DisplayName("Log out and return to the login page")
+    public void logoutGetSuccess() {
+        LoginPage loginPage = new LoginPage(driver);
+        ProfilePage profilePage = new ProfilePage(driver);
+
+        loginPage.clickProfileButton();
+        loginPage.loginUser(user);
+        profilePage.clickExitButton();
+
+        MatcherAssert.assertThat(loginPage.getLoginTextFromHeader(), equalTo("Вход"));
+    }
+
+    @After
+    public void teardown() {
+        UserOperations.deleteUser(UserOperations.getAccessToken(user));
+        driver.quit();
+    }
 }
