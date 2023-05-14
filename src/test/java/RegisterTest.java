43b6f5587c6408a70f0afcd382d05810bdde723c
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-14 17:54:40 +0400

    Created Register page and register tests

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,15 +1,16 @@
-package register;
-
 import config.AppConfig;
-import config.Generator;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
+import pages.ProfilePage;
+import user.User;
+import user.UserOperations;
+import utils.Generator;
 import config.WebDriverConfig;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
 import pages.RegisterPage;
 
@@ -19,43 +20,51 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class RegisterTest {
     private WebDriver driver;
+    User user;
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        user = Generator.generateUser();
+
+        driver = WebDriverConfig.setDriver();
         driver.manage().timeouts().implicitlyWait(WebDriverConfig.WAIT_SEC_TIMEOUT, TimeUnit.SECONDS);
         driver.navigate().to(AppConfig.REGISTER_PAGE_URL);
     }
 
     @Test
-    public void registerNewUserGetSuccess() {
-        RegisterPage registerPage = new RegisterPage(driver);
-        LoginPage loginPage = new LoginPage(driver);
-
-        registerPage.setName(Generator.generateUserName());
-        registerPage.setEmail(Generator.generateUserEmail());
-        registerPage.setPassword(Generator.generateUserPassword());
-        registerPage.clickRegisterButton();
-
-        System.out.println(loginPage.getTextFromHeader());
-        MatcherAssert.assertThat(loginPage.getTextFromHeader(), equalTo("Вход"));
-    }
-
-    @Test
+    @DisplayName("Register a new user with a short password")
     public void registerNewUserWithShortPasswordGetError() {
         RegisterPage registerPage = new RegisterPage(driver);
 
-        registerPage.setName(Generator.generateUserName());
-        registerPage.setEmail(Generator.generateUserEmail());
+        registerPage.setName(user.getName());
+        registerPage.setEmail(user.getEmail());
         registerPage.setPassword(Generator.generateWrongUserPassword());
         registerPage.clickRegisterButton();
 
         MatcherAssert.assertThat(registerPage.getInvalidPasswordText(), equalTo("Некорректный пароль"));
     }
 
+    @Test
+    @DisplayName("Register a new user with valid creds")
+    public void registerNewUserGetSuccess() {
+        RegisterPage registerPage = new RegisterPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        ProfilePage profilePage = new ProfilePage(driver);
+
+        registerPage.setName(user.getName());
+        registerPage.setEmail(user.getEmail());
+        registerPage.setPassword(user.getPassword());
+        registerPage.clickRegisterButton();
+
+        MatcherAssert.assertThat(loginPage.getLoginTextFromHeader(), equalTo("Вход"));
+        loginPage.loginUser(user);
+        Assert.assertTrue(profilePage.btnProfileTabIsEnabled());
+
+    }
+
     @After
     public void teardown() {
+        UserOperations.deleteUser(UserOperations.getAccessToken(user));
         driver.quit();
     }
 }
