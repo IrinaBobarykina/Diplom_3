fccb63a2e1fcd1d1bb0bdba53848f3058ce50ed8
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-14 17:49:58 +0400

    Created APIs.java

--- src/main/java/config/APIs.java
+++ src/main/java/config/APIs.java
@@ -1,2 +1,11 @@
-package config;public class APIs {
+package config;
+
+public class APIs {
+
+    //    создание юзера POST
+    public static final String REGISTER_PATH = "/api/auth/register";
+    //    логин юзера POST
+    public static final String LOGIN_PATH = "/api/auth/login";
+    //    удаление юзера POST
+    public static final String DELETE_PATH = "/api/auth/user";
 }
