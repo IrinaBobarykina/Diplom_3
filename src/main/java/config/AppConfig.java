0fb26466904a9a7c5bda01f35869331ee3c13150
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-14 17:51:11 +0400

    Created configs

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,8 +3,7 @@ package config;
 public class AppConfig {
     public static final String DOMAIN = "https://stellarburgers.nomoreparties.site";
     public static final String MAIN_PAGE_URL = DOMAIN + "/";
-    public static final String LOGIN_PAGE_URL = DOMAIN + "/login";
     public static final String REGISTER_PAGE_URL = DOMAIN + "/register";
-    public static final String PROFILE_PAGE_URL = DOMAIN + "/account/profile";
+    public static final String RESET_PASSWORD_PAGE_URL = DOMAIN + "/forgot-password";
 
 }
