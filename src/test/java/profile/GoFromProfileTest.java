d7359120a2c59d6446102e93641c916960748f1c
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-05-14 17:55:24 +0400

    Added Profile page and profile tests

--- src/test/java/profile/GoFromProfileTest.java
+++ src/test/java/profile/GoFromProfileTest.java
@@ -1,27 +1,23 @@
 package profile;
 
 import config.AppConfig;
-import config.BackToMainDetails;
-import config.Generator;
+import io.qameta.allure.junit4.DisplayName;
+import utils.BackToMainDetails;
 import config.WebDriverConfig;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.LoginPage;
 import pages.MainPage;
-import pages.ProfilePage;
-import pages.RegisterPage;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
+@RunWith(Parameterized.class)
 public class GoFromProfileTest {
 
     private WebDriver driver;
@@ -29,8 +25,7 @@ public class GoFromProfileTest {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = WebDriverConfig.setDriver();
         driver.manage().timeouts().implicitlyWait(WebDriverConfig.WAIT_SEC_TIMEOUT, TimeUnit.SECONDS);
         driver.navigate().to(AppConfig.MAIN_PAGE_URL);
     }
@@ -39,8 +34,9 @@ public class GoFromProfileTest {
         this.button = button;
     }
 
-    @Parameterized.Parameters
-    public static Object[] orderScooter() {
+
+    @Parameterized.Parameters(name = "Go from profile using the {0} button")
+    public static Object[] backToMainButtons() {
         return new Object[][]{
                 {BackToMainDetails.LOGO_BACK_TO_MAIN_PAGE},
                 {BackToMainDetails.CONSTRUCTOR_BACK_TO_MAIN},
@@ -48,8 +44,10 @@ public class GoFromProfileTest {
     }
 
     @Test
-    public void registerNewUserGetSuccess() {
+    @DisplayName("Go from the profile to the main page")
+    public void goFromProfileToMain() {
         MainPage mainPage = new MainPage(driver);
+
         mainPage.clickProfileButton();
         mainPage.backToMainPage(button);
 
@@ -60,5 +58,4 @@ public class GoFromProfileTest {
     public void teardown() {
         driver.quit();
     }
-
 }
--- src/test/java/profile/GoToProfileTest.java
+++ src/test/java/profile/GoToProfileTest.java
@@ -1,2 +1,68 @@
-package profile;public class GoToProfileTest {
+package profile;
+
+import config.AppConfig;
+import config.BaseURI;
+import config.WebDriverConfig;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.ProfilePage;
+import user.User;
+import user.UserOperations;
+import utils.Generator;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class GoToProfileTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = BaseURI.BASE_URI;
+        driver = WebDriverConfig.setDriver();
+        driver.manage().timeouts().implicitlyWait(WebDriverConfig.WAIT_SEC_TIMEOUT, TimeUnit.SECONDS);
+        driver.navigate().to(AppConfig.MAIN_PAGE_URL);
+    }
+
+    @Test
+    @DisplayName("Go to the profile as an authorized user")
+    public void goToProfileAuthUserGetProfile() {
+        User user = Generator.generateUser();
+        MainPage mainPage = new MainPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        ProfilePage profilePage = new ProfilePage(driver);
+
+        UserOperations.createUser(user);
+        mainPage.clickProfileButton();
+        loginPage.loginUser(user);
+
+        Assert.assertTrue(profilePage.btnProfileTabIsEnabled());
+        UserOperations.deleteUser(UserOperations.getAccessToken(user));
+    }
+
+    @Test
+   @DisplayName("Go to the profile as an unauthorized user")
+    public void goToProfileUnauthUserGetLogin() {
+        MainPage mainPage = new MainPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+
+        mainPage.clickProfileButton();
+
+        MatcherAssert.assertThat(loginPage.getLoginTextFromHeader(), equalTo("Вход"));
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
 }
